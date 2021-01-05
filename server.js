6b9e0b9953a9661484cdb4e70fd0e163eb454143
codabool <codabool@pm.me>
2021-01-05 05:40:24 -0500

    asd

--- server.js
+++ server.js
@@ -28,21 +28,6 @@ async function query(q, values, p) {
     })
 }
 
-// app.get('/blog', async (req, res) => {
-//   try {
-//     const result = await query('SELECT * FROM post', [], pool_remote)
-//     let totalViews = 0
-//     for (const page in result.rows) {
-//       console.log(totalViews, ' + ', Number(result.rows[page].views))
-//       totalViews = totalViews + Number(result.rows[page].views)
-//     }
-//     const inReview = await query("SELECT * FROM comment WHERE status='review'", [], pool_remote)
-//     res.status(200).json({stat: result.rows, inReview: inReview.rows, totalViews})
-//   } catch (err) {
-//     res.status(400).send('General Error Cannot Stats')
-//   }
-// });
-
 app.get('/', async (req, res) => {
   try {
     // remote pg connection
